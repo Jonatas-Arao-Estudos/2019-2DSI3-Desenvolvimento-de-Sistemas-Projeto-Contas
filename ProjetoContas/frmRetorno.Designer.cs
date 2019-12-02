d90f1965ed9ab9310612616ebf73ea6debb1731d
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2019-12-02 19:40:38 -0300

    frmRetorno - Form

---
+++ ProjetoContas/frmRetorno.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ProjetoContas
+{
+    partial class frmRetorno
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtArquivo = new System.Windows.Forms.TextBox();
+            this.contasDataSet = new ProjetoContas.contasDataSet();
+            this.tbContasReceberBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tbContasReceberTableAdapter = new ProjetoContas.contasDataSetTableAdapters.tbContasReceberTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.contasDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbContasReceberBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Corbel", 14F);
+            this.btnSair.Location = new System.Drawing.Point(283, 113);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(98, 38);
+            this.btnSair.TabIndex = 7;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Font = new System.Drawing.Font("Corbel", 14F);
+            this.btnOK.Location = new System.Drawing.Point(167, 113);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(98, 38);
+            this.btnOK.TabIndex = 6;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Corbel", 15F);
+            this.label1.Location = new System.Drawing.Point(12, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Arquivo";
+            // 
+            // txtArquivo
+            // 
+            this.txtArquivo.Font = new System.Drawing.Font("Corbel", 15F);
+            this.txtArquivo.Location = new System.Drawing.Point(93, 62);
+            this.txtArquivo.Name = "txtArquivo";
+            this.txtArquivo.Size = new System.Drawing.Size(422, 32);
+            this.txtArquivo.TabIndex = 8;
+            // 
+            // contasDataSet
+            // 
+            this.contasDataSet.DataSetName = "contasDataSet";
+            this.contasDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tbContasReceberBindingSource
+            // 
+            this.tbContasReceberBindingSource.DataMember = "tbContasReceber";
+            this.tbContasReceberBindingSource.DataSource = this.contasDataSet;
+            // 
+            // tbContasReceberTableAdapter
+            // 
+            this.tbContasReceberTableAdapter.ClearBeforeFill = true;
+            // 
+            // frmRetorno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 186);
+            this.Controls.Add(this.txtArquivo);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Corbel", 8.25F);
+            this.Name = "frmRetorno";
+            this.Text = "frmRetorno";
+            this.Load += new System.EventHandler(this.frmRetorno_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.contasDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbContasReceberBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtArquivo;
+        private contasDataSet contasDataSet;
+        private System.Windows.Forms.BindingSource tbContasReceberBindingSource;
+        private contasDataSetTableAdapters.tbContasReceberTableAdapter tbContasReceberTableAdapter;
+    }
+}
---
+++ ProjetoContas/frmRetorno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ProjetoContas
+{
+    public partial class frmRetorno : Form
+    {
+        public frmRetorno()
+        {
+            InitializeComponent();
+        }
+
+        private void tbContasReceberBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.tbContasReceberBindingSource.EndEdit();
+
+        }
+
+        private void frmRetorno_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'contasDataSet.tbContasReceber'. Você pode movê-la ou removê-la conforme necessário.
+            this.tbContasReceberTableAdapter.Fill(this.contasDataSet.tbContasReceber);
+
+        }
+    }
+}
